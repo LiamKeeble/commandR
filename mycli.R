f2ea5c0f28d0bcb14c5a1f47567c44ee3f57d443
Liam Keeble <liamkeeble@gmail.com>
2023-03-22 10:14:28 +0000

    Added some more test datasets and model family option

--- mycli.R
+++ mycli.R
@@ -5,12 +5,26 @@ library(argparser)
 p <- arg_parser("Command Line Models")
 p <- add_argument(p, "model", help="Model formula")
 p <- add_argument(p, "data", help="Data file")
+p <- add_argument(p, "family", help="Family, e.g. binomial")
 argv <- parse_args(p)
 
 
-
 data=read.csv(argv$data)
-summary(lm(argv$model, data))
+
+if (argv$family == "binomial") {
+
+	summary(glm(argv$model, data, family = argv$family))
+
+} else if (argv$family == "poisson") {
+
+	summary(glm(argv$model, data, family = argv$family))
+} else {
+
+	summary(lm(argv$model, data))
+
+}
+
+
 
 
 
--- test.R
+++ test.R
@@ -12,3 +12,23 @@ df = data.frame(
 write.csv(df, "data.csv")
 
 
+
+poisson = data.frame(
+
+		     x = rpois(100,1),
+		     y = rpois(100,1)
+
+)
+
+write.csv(poisson, "poisson.csv")
+
+binomial = data.frame(
+
+		      x = rbinom(100, 1, 0.8),
+		      y = rbinom(100, 1, 0.6)
+)
+
+
+write.csv(binomial, "binomial.csv")
+
+
